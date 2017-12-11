b7424f43df7590c4793b2c1c6ce878b55f563e0a
divinediscipline <divinediscipline4real@gmail.com>
2017-12-12 00:05:19 +0100

    Added GET /todos;id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,8 +30,23 @@ app.get("/todos", (req, res) => {
   });
 });
 
+app.get("/home", (req, res) => {
+ var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send(todo);    
+  }).catch((e) => res.status(400).send());
+
+});
+
 app.listen(3000, () => {
-  console.log('Started on port 3000');
+  console.log('App Started on port 3000');
 });
 
 module.exports = {app};
